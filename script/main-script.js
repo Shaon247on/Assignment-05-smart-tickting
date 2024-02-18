19aa224757d49685e8cd4993f7d6308df631cd72
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-02-19 02:21:21 +0600

    added the coupon input and button

--- script/main-script.js
+++ script/main-script.js
@@ -1,42 +1,58 @@
-const allSelector= document.getElementsByClassName('selector')
+const allSelector = document.getElementsByClassName('selector')
 let count = 40;
-let booked= 0;
-// let cost =0;
-for(const btn of allSelector){
-    btn.addEventListener('click', function(e){
+let booked = 0;
+for (const btn of allSelector) {
+    btn.addEventListener('click', function (e) {
         btn.classList.toggle('bg-[#1DD100]')
-        count-=1
+        count -= 1
         setInnerText('available-seat', count)
-        booked+=1
-        setInnerText('booked-seat',booked)
-        // console.log(count)
+        booked += 1
+        setInnerText('booked-seat', booked)
         const seatName = e.target.innerText
         const seatType = 'Economy'
-        const seatPrice = 50
+        const seatPrice = 550
         const accountContainer = document.getElementById('account-container')
-        const tr =document.createElement('tr')
-        const td =document.createElement('td')
+        const tr = document.createElement('tr')
+        const td = document.createElement('td')
         td.classList.add('text-start')
         td.innerText = seatName
-        const td2 =document.createElement('td')
-        td2.innerText= seatType
-        const td3 =document.createElement('td')
+        const td2 = document.createElement('td')
+        td2.innerText = seatType
+        const td3 = document.createElement('td')
         td3.classList.add('text-end')
-        td3.innerText=seatPrice
+        td3.innerText = 550
         tr.appendChild(td)
         tr.appendChild(td2)
         tr.appendChild(td3)
         accountContainer.appendChild(tr)
-        // cost+=550
-        // setInnerText('total-amount',cost)
+        totalCost('total-amount', seatPrice)
 
-        
-    })    
+        totalCost('grand-total', seatPrice)
+    })
 }
 
+function totalCost(id, value) {
+    const totalAmount = document.getElementById(id).innerText;
+    const convertedTotalAmount = parseInt(totalAmount)
+    const sum = convertedTotalAmount + value
+    setInnerText(id, sum)
+}
 
-
-function setInnerText(id, value){
+function setInnerText(id, value) {
     const availableSeat = document.getElementById(id)
-    availableSeat.innerText=value
+    availableSeat.innerText = value
 }
+const input = document.getElementById('coupon-input')
+const coupneButton = document.getElementById('coupon-button')
+input.addEventListener('input',function(){
+    if (input.value === 'NEW15'|| input.value === 'Couple 20') {
+        coupneButton.removeAttribute('disabled')
+        coupneButton.addEventListener('click', function () {
+                coupneButton.classList.add('hidden')
+                input.classList.add('hidden')
+    })}
+    else{
+        coupneButton.setAttribute('disabled', 'true')
+    }
+    
+})
